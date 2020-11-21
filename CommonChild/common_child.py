8d3124e174ca52933e8f4998d86d2aa1cb462432
Iñaki Silanes <isilanes@gmail.com>
2020-11-21 17:27:37 +0100

    CommonChild: faster method, still too slow

--- CommonChild/common_child.py
+++ CommonChild/common_child.py
@@ -1,33 +1,44 @@
-def find_pair(s1, s2, p1, p2):
+def find_pair(s1, d1, p1, p2):
     for i, c in enumerate(s1[p1:]):
-        try:
-            j = s2.index(c, p2)
-            return p1 + i, j
-        except ValueError:
-            pass
+        for j in d1[c]:
+            if j >= p2:
+                return p1 + i, j
 
     return None, None
 
 
+def build_dict(s1, s2):
+    output = {}
+    for c in s1:
+        if c not in output:
+            output[c] = []
+            p2 = 0
+            while True:
+                try:
+                    p2 = s2.index(c, p2)
+                    output[c].append(p2)
+                    p2 += 1
+                except ValueError:
+                    break
+
+    return output
+
+
 def commonChild(s1, s2):
+    d1 = build_dict(s1, s2)
+
     word_indices1, word_indices2 = [], []
-    mwi = []
     max_len = 0
     p1, p2 = 0, 0
     while True:
-        pre1 = p1
-        pre2 = p2
-
-        p1, p2 = find_pair(s1, s2, p1, p2)
+        p1, p2 = find_pair(s1, d1, p1, p2)
 
         if p1 is None:
             if not word_indices1:
                 return max_len
 
             if len(word_indices1) > max_len:
-                mwi = word_indices1
                 max_len = len(word_indices1)
-
 
             p1 = word_indices1[-1] + 1
             word_indices1 = word_indices1[:-1]
@@ -46,19 +57,21 @@ def commonChild(s1, s2):
             p2 += 1
 
 
+def test1():
+    assert commonChild("HARRY", "SALLY") == 2
+    assert commonChild("AA", "BB") == 0
+    assert commonChild("SHINCHAN", "NOHARAAA") == 3
 
-assert commonChild("HARRY", "SALLY") == 2
-assert commonChild("AA", "BB") == 0
-assert commonChild("SHINCHAN", "NOHARAAA") == 3
 
-with open("input01.txt") as f:
-    s1 = f.readline().strip()
-    s2 = f.readline().strip()
+def test_fn(fn, expected):
+    with open(fn) as f:
+        s1 = f.readline().strip()
+        s2 = f.readline().strip()
 
-assert commonChild(s1, s2) == 15
+    assert commonChild(s1, s2) == expected
 
-with open("input10.txt") as f:
-    s1 = f.readline().strip()
-    s2 = f.readline().strip()
 
-assert commonChild(s1, s2) == 1618
+if __name__ == "__main__":
+    test1()
+    test_fn("input01.txt", 15)
+    #test_fn("input10.txt", 1618)
